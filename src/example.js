0243cf62f40c3f6b5efd917f0df38693ff705005
Reuben Berghan <reuben.berghan@gmail.com>
2019-12-15 15:04:19 +1300

    chore: add config

---
+++ src/example.js
@@ -0,0 +1,11 @@
+const username = 'freddy'
+typeof username === 'string'
+
+if (!('serviceWorker' in navigator)) {
+  // old browser :(
+}
+
+const greeting = 'hello'
+console.log(`${greeting} world!`)
+// prettier-ignore
+;[1, 2, 3].forEach(x => console.log(x))
